eac6841f82b0705a2a3bd1cc92df56650478973a
Ade Prast <adecma18@gmail.com>
2016-11-22 11:55:40 +0800

    edit text homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <i class="fa fa-users"></i>&nbsp;Pemuda App
+                    <i class="fa fa-users"></i>&nbsp;Generus App
                 </div>
             </div>
         </div>
